624bd1bac5a54c14b2fd767ac088402c5130aab5
naveenkarri383 <karrinaveen.mme@gmail.com>
2024-02-10 02:06:56 +0530

    Logger added

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-from src.ML_Project.config import configuration  # Importing main function from the configuration.py file
+#from src.ML_Project.config import configuration  # Importing main function from the configuration.py file
+from src.ML_Project import logger
+
+logger.info("Successfully executed your code!")
